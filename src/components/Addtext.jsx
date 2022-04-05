78d5d56ff011cad5945128182761ee037959ffe3
skmuneer <muneer@nuevesolutions.com>
2022-04-05 14:54:45 +0530

    added alerts and changed favicon.ioc

--- src/components/Addtext.jsx
+++ src/components/Addtext.jsx
@@ -10,32 +10,46 @@ var BACKEND_URL =
 
 const Addtext = () => {
     const [data,setData]=useState("")
-    const [text,setText]=useState("ADD A WORD TO DICTONARY")
+    // const[color1,setColor1] =useState("red")
+    // const [err,setErr]=useState("")
     const add=(e)=>{
         e.preventDefault();
+        if(data!=""){
         var body={
             word1:data,
         }
+        // setColor1("green")
+        // setErr("SuccessFully Inserted")
         Axios.post(`${BACKEND_URL}/add`,body)
         .then((response)=>{
             console.log(response,"res")
+        alert("SUCCESSFULLY INSERTED")
+
+             setData("")
         })
         .catch((err)=>{
             console.log(err,"err")
         })
+
     }
+    else{
+        alert("TEXTFIELD CANNOT BE EMPTY")
+        // setErr("text field cannot be empty")
+    }
+}
     useEffect(()=>{
 
     },[data])
   return (
     <div>
         <div>
-            <h1> {text}</h1>
+            <h1> ADD A WORD TO DICTONARY</h1>
           
                 {/* <input type="text" value={data} onChange={e=>setData(e.target.value)}/> */}
                 <TextField id="filled-basic" style={{marginBottom:"20px"}} label="ENTER A WORD" variant="filled" value={data} onChange={e=>setData(e.target.value)} />
-    
-          <br/>
+              <br/>
+              {/* <p style={{color: {color1}}}>{data ? "" : err}</p> */}
+          
             
 {/* <button type="button" onClick={add}>Add</button> */}
 <Button variant="contained" onClick={add}> Add Text</Button>
